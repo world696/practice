d3404efc71ec4fc3198a67d570c3d6d85c4ca2fc
qiyan <qiyan@keenon.com>
2023-09-07 16:53:26 +0800

    feat：【字符串】无重复字符的最长子串

---
+++ 3.js
@@ -0,0 +1,22 @@
+
+/**
+ * 获取最长字串长度
+ * @param {*} str 字符串
+ * @returns number 字符串长度
+ * 时间复杂度 
+ * charAt 返回字符串中的第x个字符
+ */
+const lengthOfLongstring = function(str) {
+    let arr = [], max = 0
+    for(let i = 0; i< str.length; i++) {
+        let index = arr.indexOf(str[i])
+        if (index !== -1) {
+            arr.splice(0, index + 1)
+        }
+        arr.push(str.charAt(i))
+        max = Math.max(arr.length, max)
+    }
+    return max
+}
+
+console.log(lengthOfLongstring('abcabcbb'));
