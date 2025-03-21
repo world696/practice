5e9b9e52c0d6932e75f3f3dff0b6f17f4516e947
qiyan <qiyan@keenon.com>
2025-03-21 15:41:07 +0800

    test

--- 0026.js
+++ 0026.js
@@ -1,12 +1,14 @@
 /**
- * 【】
+ * 【26】
  * @param {number[]} nums
  * @return {number}
  * 【数组】去重
+ * 有个地方需要注意一下，是原地操作，相当于不创建新的内存去存储，直接修改原数组
+ * 
  */
 const removeDuplicates = function(nums) {
   
 }
 
-// console.log(removeDuplicates([1,1,2]));
-// console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
---
+++ basic/TypeScript/0001/0001.js
@@ -0,0 +1,5 @@
+/**
+ * 基本类型
+ * 1) 原始类型: string, number, boolean
+ * 
+ */
