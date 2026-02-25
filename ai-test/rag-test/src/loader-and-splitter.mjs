d95ee36b5c4824b997875a5a460afc737283e55c
world_66 <1509732576@qq.com>
2026-02-25 22:17:37 +0800

    feat: loader and splitter

---
+++ ai-test/rag-test/src/loader-and-splitter.mjs
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import "cheerio";
+import { CheerioWebBaseLoader } from"@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'
+
+const cheerioLoader = new CheerioWebBaseLoader(
+"https://juejin.cn/post/7233327509919547452",
+  {
+    selector: '.main-area p'
+  }
+);
+
+const documents = await cheerioLoader.load();
+
+// 每个文档是都是 400 字符左右，前后重复了 50 个字符。
+const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 400,  // 每个分块的字符数
+    chunkOverlap: 50,  // 分块之间的重叠字符数
+    separators: ["。","！","？"],  // 分割符，优先使用段落分隔
+});
+const splitDocuments = await textSplitter.splitDocuments(documents)
+// console.log(documents);
+console.log(splitDocuments);
